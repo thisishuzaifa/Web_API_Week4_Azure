8bfa381c6b9e4f27ae463bdf9cf063b16a8ee0cc
Huzaifa Khalid <huzaifa_khalid97@outlook.com>
2022-02-25 05:26:44 -0800

    Add project files.

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Web_API_02.Database_Helper;
+
+namespace Web_API_02.Controllers
+//namespace Web_API_day_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        // GetAll() is automatically recognized as
+        // http://localhost:<port #>/api/todo
+        [HttpGet]
+        public IEnumerable<ToDo> GetAll()
+        {
+            return _context.ToDos.ToList();
+        }
+
+        // GetById() is automatically recognized as
+        // http://localhost:<port #>/api/todo/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetById(long id)
+        {
+            var item = _context.ToDos.FirstOrDefault(t => t.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] ToDo todo)
+        {
+            if (todo.Description == null || todo.Description == "")
+            {
+                return BadRequest();
+            }
+            _context.ToDos.Add(todo);
+            _context.SaveChanges();
+            return new ObjectResult(todo);
+        }
+
+        [HttpPut]
+        [Route("myedit")] // Custom route
+        public IActionResult GetByParams([FromBody] ToDo todo)
+        {
+            var item = _context.ToDos.Where(t => t.Id == todo.Id).FirstOrDefault();
+            if (item == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                item.Description = todo.Description;
+                item.IsComplete = todo.IsComplete;
+                _context.SaveChanges();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult MyDelete(int id)
+        {
+            var item = _context.ToDos.Where(t => t.Id == id).FirstOrDefault();
+            if (item == null)
+            {
+                return NotFound();
+            }
+            _context.ToDos.Remove(item);
+            _context.SaveChanges();
+            return new ObjectResult(item);
+        }
+    }
+}
---
+++ Migrations/20220225132455_initialCreate.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Web_API_02.Migrations
+{
+    public partial class initialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    IsComplete = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Priority = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDos", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ToDos",
+                columns: new[] { "Id", "Description", "IsComplete", "Priority" },
+                values: new object[] { 1, "Clean house", false, 1 });
+
+            migrationBuilder.InsertData(
+                table: "ToDos",
+                columns: new[] { "Id", "Description", "IsComplete", "Priority" },
+                values: new object[] { 2, "Bake cake", false, 3 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDos");
+        }
+    }
+}
